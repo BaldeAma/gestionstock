9d7218638da1f3480961e699613c87bd3e500b9a
BaldeAma <amadoubalde229@gmail.com>
2026-02-12 22:59:22 +0100

    initial

---
+++ src/main/java/com/obi/gestiondesstock/model/Article.java
@@ -0,0 +1,15 @@
+package com.obi.gestiondesstock.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "article" )
+public class Article extends AbstractEntity{
+}
---
+++ src/main/java/com/obi/gestiondesstock/model/Utilisateur.java
@@ -0,0 +1,16 @@
+package com.obi.gestiondesstock.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "utilisateur" )
+public class Utilisateur extends AbstractEntity {
+}
